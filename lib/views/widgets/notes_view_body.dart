2f7ac2cd08b6e697d675ae209a8ff24f7a741718
yasserpro2004 <yassersayed2003@hotmail.com>
2023-03-09 21:14:48 +0200

    Create note Items

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'custom_app_bar.dart';
+import 'custom_note_Item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
